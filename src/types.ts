14522c1d19390f54a6df944a53888d8c1e76afd1
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2025-10-31 01:22:39 +0800

    feat: add types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type ResidencyLog = {
+  id: number;
+  student_uid: string;
+  time_in: Date;
+  time_out: Date;
+  residency_type: "core" | "ancilliary";
+}
+
+export type Student = {
+  id: number;
+  student_uid: string;
+  name: string;
+  committee: 
+    "customerCare" |
+    "layout"       |
+    "literary"     |
+    "marketing"    |
+    "office"       |
+    "photo"        |
+    "web"
+  ; // committee_type
+}
