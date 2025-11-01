1261c93f302c8d42bb61be17cf7f2c1c1f0dc33f
Alvinator24 <alvinalvin3065@gmail.com>
2025-11-01 13:07:08 +0800

    feat: add code entry page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Main from './Pages/Main/page.tsx'
 import Residency from './Pages/Residency/page.tsx'
 import PublicView from './Pages/PublicView/page.tsx'
+import EnterCode from './Pages/EnterCode/page.tsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<Main/>}/>
           <Route path='/residency' element={<Residency/>}/>
           <Route path='/publicview' element={<PublicView/>}/>
+          <Route path='/entercode' element={<EnterCode/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Pages/EnterCode/page.tsx
@@ -0,0 +1,13 @@
+import gwLogo from '../../assets/gw_logo.png'
+
+export default function EnterCode() {
+    return (
+        <main className='flex flex-col justify-center items-center min-h-screen space-y-4'>
+            <img src={gwLogo} alt="gwLogo" className='h-32 w-auto' />
+            <div>
+                <input type="password" placeholder='Enter code here' className='border rounded-sm px-2 py-1' required/>
+                <button className='m-2 border rounded-sm p-1 cursor-pointer hover:bg-gray-100 transition'>Submit</button>
+            </div>
+        </main>
+    )
+}
