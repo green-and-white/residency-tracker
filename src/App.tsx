bcbe7ed60899fd4879f581b0aa8baaa528300259
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2025-11-15 13:53:42 +0800

    refactor: remove get code page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Main from './Pages/Main/page.tsx'
 import Residency from './Pages/Residency/page.tsx'
 import PublicView from './Pages/PublicView/page.tsx'
 import Admin from './Pages/Admin/page.tsx'
-import GetCode from './Pages/GetCode/page.tsx'
 import ProtectedRoute from './components/ProtectedRoute.tsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { SessionProvider } from './components/ui/auth.tsx'
@@ -18,7 +17,6 @@ function App() {
             <Route path='/residency' element={<ProtectedRoute><Residency/></ProtectedRoute>}/>
             <Route path='/publicview' element={<PublicView/>}/>
             <Route path='/admin' element={<Admin/>}/>
-            <Route path='/getcode' element={<ProtectedRoute><GetCode/></ProtectedRoute>} />
           </Routes>
         </BrowserRouter>
       </SessionProvider>
