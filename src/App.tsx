a41c58f4521426922ad3790c14d372bf7513064b
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2025-11-15 13:35:59 +0800

    refactor: remove entercode page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import Main from './Pages/Main/page.tsx'
 import Residency from './Pages/Residency/page.tsx'
 import PublicView from './Pages/PublicView/page.tsx'
-import EnterCode from './Pages/EnterCode/page.tsx'
 import Admin from './Pages/Admin/page.tsx'
 import GetCode from './Pages/GetCode/page.tsx'
 import ProtectedRoute from './components/ProtectedRoute.tsx'
@@ -18,7 +17,6 @@ function App() {
             <Route path='/' element={<Main/>}/>
             <Route path='/residency' element={<ProtectedRoute><Residency/></ProtectedRoute>}/>
             <Route path='/publicview' element={<PublicView/>}/>
-            <Route path='/entercode' element={<EnterCode/>}/>
             <Route path='/admin' element={<Admin/>}/>
             <Route path='/getcode' element={<ProtectedRoute><GetCode/></ProtectedRoute>} />
           </Routes>
