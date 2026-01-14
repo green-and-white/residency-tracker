6cadd3082e9f3d57b2869ffd3a3a35bd6bc4899b
Alvinator24 <alvinalvin3065@gmail.com>
2026-01-14 23:57:53 +0800

    chore: comment out admin page import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Main from './Pages/Main/page.tsx'
 import Residency from './Pages/Residency/page.tsx'
 import PublicView from './Pages/PublicView/page.tsx'
-import Admin from './Pages/Admin/page.tsx'
+// import Admin from './Pages/Admin/page.tsx'
 import ProtectedRoute from './components/ProtectedRoute.tsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { SessionProvider } from './components/ui/auth.tsx'
